654cc6908ad63fbcb7a1a77158a46572a28e9135
kayky-wictor <kaykywictor5@gmail.com>
2025-11-12 20:19:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] vetor = { "Kayky", "Jungkook", "Jhope", "Jin", "Namjoom", "JIn", "BTs", "Yoongi", "Jimin", "Taehyung" };
+
+        
+        Console.Write("Digite o nome ou número que deseja buscar: ");
+        string busca = Console.ReadLine();
+
+        
+        bool encontrado = false;
+
+        
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i].Equals(busca, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"'{busca}' encontrado na posição {i} do vetor.");
+                encontrado = true;
+                break; 
+            }
+        }
+
+        // Caso não tenha encontrado
+        if (!encontrado)
+        {
+            Console.WriteLine("Valor não encontrado no vetor.");
+        }
+
+        Console.ReadKey();
+    }
+}
